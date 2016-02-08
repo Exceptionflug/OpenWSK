7f708c3545dd6a65335ed9b3970003f4d069f488
MrCreeperkopf <dietntexperten@gmail.com>
2016-02-08 13:45:13 +0100

    This class contains the /wsk commands.

--- src/net/thecobix/openwsk/commands/CommandWSK.java
+++ src/net/thecobix/openwsk/commands/CommandWSK.java
@@ -6,6 +6,23 @@ import de.pro_crafting.commandframework.Command;
 import de.pro_crafting.commandframework.CommandArgs;
 import net.thecobix.openwsk.main.OpenWSK;
 
+/*
+ * OpenWSK WarShip Fight System by St0n3gr1d
+   Copyright (C) 2016 St0n3gr1d
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 public class CommandWSK {
 
 	@Command(name="wsk", description="Zeigt Informationen über das Plugin an.", usage="/wsk")
