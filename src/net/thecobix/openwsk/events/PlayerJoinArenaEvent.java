32bcb4769ca2f9c819d371c40b9f3120a66f596e
MrCreeperkopf <dietntexperten@gmail.com>
2016-02-08 13:47:10 +0100

    Events for player leave/join a arena

---
+++ src/net/thecobix/openwsk/events/PlayerJoinArenaEvent.java
@@ -0,0 +1,34 @@
+package net.thecobix.openwsk.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import net.thecobix.openwsk.arena.Arena;
+
+public class PlayerJoinArenaEvent extends Event {
+
+	private static final HandlerList handlers = new HandlerList();
+	
+	private Arena arena;
+	private Player player;
+	
+	public PlayerJoinArenaEvent(Arena arena, Player player) {
+		this.arena = arena;
+		this.player = player;
+	}
+	
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public Arena getArena() {
+		return arena;
+	}
+	
+	public Player getPlayer() {
+		return player;
+	}
+	
+}
